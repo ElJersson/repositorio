ae585abc5ee29f20b3a073207412c2a46f05452f
Jersson_Frias <yeyfri76@gmail.com>
2023-11-30 16:19:29 -0500

    curentayunavo

--- frontend/src/routes/url.js
+++ frontend/src/routes/url.js
@@ -1,4 +1,4 @@
 //export const  url='`${url}`'
-export const URL="http://localhost:4500"
-// export const URL="https://repositorio.onrender.com"
+// export const URL="http://localhost:4500"
+export const URL="https://repositorio.onrender.com"
                                                                                  
